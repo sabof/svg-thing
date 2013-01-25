89a6b069f8c92001b2057168b4664fafb4050226
sabof <sabof@example.com>
2013-01-25 23:07:52 +0000

    productization

--- svg-thing.el
+++ svg-thing.el
@@ -1,10 +1,11 @@
-(defvar is-st nil)
-(make-variable-buffer-local 'is-st)
+(defvar is-svg-thing nil)
+(make-variable-buffer-local 'is-svg-thing)
 (defvar st-timer nil)
 (defvar st-mouse-down-pos nil)
 (defvar st-objects nil)
 (defvar st-drag-timer nil)
 (defvar st-drag-object nil)
+(defvar st-name nil)
 
 (defun st-mouse-position ()
   (let ((wpe (window-inside-pixel-edges))
@@ -36,6 +37,27 @@
                    " L"))))
     (format "<path d=\"%sZ\" fill=\"none\" %s />"
             line-positions (or extra-props ""))))
+
+(defun st-center-text (text)
+  (let* ((wpe (window-inside-pixel-edges))
+         (width (- (third wpe) (first wpe)))
+         (height (- (fourth wpe) (second wpe)))
+         (font-size 100))
+    (concat
+     (format "<g transform=\"translate(%s %s)\">"
+             (/ width 2)
+             (/ (+ height font-size) 2))
+     (format "<rect width=\"%s\" height=\"%s\" x=\"%s\" y=\"%s\" fill=\"none\"/>
+<text text-anchor=\"middle\" dominant-baseline=\"mathematical\" fill=\"#888\" fill-opacity=\"0.2\"
+style=\"font-weight:bold; font-size: %spx; font-family: sans-serif;\">%s</text>"
+             (third wpe)
+             (fourth wpe)
+             (- (/ width 2))
+             (- (/ height 2))
+             font-size
+             text)
+     "</g>"
+     )))
 
 (defun* st-curve (positions &optional extra-props)
   (when (< (length positions) 3)
@@ -84,9 +106,10 @@
 
 (defun st-set-keys ()
   (es-buffer-local-set-keys
-    (kbd "<mouse-1>") 'st-on-click
+    (kbd "<mouse-2>") 'st-on-click
     (kbd "<down-mouse-1>") 'st-mouse-down
     (kbd "<drag-mouse-1>") 'st-mouse-drag
+    (kbd "<mouse-3>") 'st-right-click
     (kbd "g") 'st-reset))
 
 (defun* st-redraw ()
@@ -95,11 +118,11 @@
            (format "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"%s\" height=\"%s\" version=\"1.1\">\n"
                    (third wpe) (fourth wpe)))
          (circles "")
-         (line1 (st-curve st-objects "stroke=\"white\" stroke-dasharray=\"5 2\" stroke-width=\"1\""))
-         (line2 (st-curve (reverse st-objects) "stroke=\"black\""))
+         (line1 (st-curve st-objects "stroke=\"#ccc\" stroke-dasharray=\"5 2\" stroke-width=\"1\""))
+         (line2 (st-curve (reverse st-objects) "stroke=\"#000\""))
+         (text (st-center-text st-name))
          (objects st-objects)
          (total-objects (length st-objects)))
-
     (dotimes (iter total-objects)
       (let* (( obj (pop objects))
              ( color
@@ -113,39 +136,45 @@
         (setq circles
               (concat
                (format
-                "<circle cx=\"%s\" cy=\"%s\" r=\"%s\" stroke=\"black\" stroke-width=\"2\" fill=\"%s\"/>\n"
+                "<circle cx=\"%s\" cy=\"%s\" r=\"%s\" stroke=\"#000\" stroke-width=\"2\" fill=\"%s\"/>\n"
                 (car obj) (cdr obj) 5 color)
                circles))))
-    (setq content (concat content line1 line2 circles "</svg>"))
+    (setq content (concat content text line1 line2 circles "</svg>"))
     (fundamental-mode)
     (erase-buffer)
     (insert content)
     (es-silence-messages
      (image-mode))))
 
-(defun st-reset-buffer ()
+(defun st-set-name ()
+  (let ((r1 (random 10))
+        (r2 (random 10)))
+    (setq st-name (format "WORK \n %s%s%s" r1 r2 r1))))
+
+(defun st-setup-buffer ()
+  (unless st-name
+    (st-set-name))
   (st-redraw)
   (es-disable-buffer-scrolling)
-  (st-set-keys))
-
-(defun st-move-object (pos new-pos)
-  )
+  (st-set-keys)
+  (setq is-svg-thing t))
 
 (defun st-reset ()
   (interactive)
   (setq st-objects nil)
-  (st-reset-buffer))
+  (st-set-name)
+  (st-setup-buffer))
 
 (defun st-drag-on-timer ()
   (when st-drag-object
     (let ((pos (st-mouse-position)))
       (setcar st-drag-object (car pos))
       (setcdr st-drag-object (cdr pos))
-      (st-reset-buffer))))
+      (st-setup-buffer))))
 
 (defun st-mouse-down (&rest ignore)
   (interactive)
-  (let ((obj ))
+  (let (obj)
     (setq st-mouse-down-pos (st-mouse-position))
     (setq obj (st-object-at st-mouse-down-pos))
     (when obj
@@ -168,16 +197,30 @@
              (last st-objects 2)))
       (es-back-push pos st-objects)))
 
+(defun st-right-click ()
+  (interactive)
+  (let ((obj (st-object-at (st-mouse-position))))
+    (when obj
+      (setq st-objects (remove obj st-objects))
+      (st-setup-buffer))))
+
 (defun st-on-click (&rest ignore)
   (interactive)
   (st-add-dot (st-mouse-position))
-  (st-reset-buffer))
+  (st-setup-buffer))
 
-(defun svg-thing-start ()
+(defun st-win-config-hook ()
+  (with-current-buffer (window-buffer)
+    (when is-svg-thing
+      (st-setup-buffer))))
+
+(defun svg-thing ()
   (interactive)
-  (switch-to-buffer "*svg-thing*")
-  (st-reset))
-
+  (switch-to-buffer " *svg-thing*")
+  (st-setup-buffer)
+  (add-hook
+   'window-configuration-change-hook
+   'st-win-config-hook))
 
 (provide 'svg-thing)
 ;; svg-thing.el ends here
